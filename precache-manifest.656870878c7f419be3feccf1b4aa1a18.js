53d65d5de8421a04e434347185d00bf1de20e8a7
Brian Lederman <brianldrmn@gmail.com>
2019-12-05 17:32:35 -0800

    Updates

---
+++ precache-manifest.656870878c7f419be3feccf1b4aa1a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a40ff031735380bc1c8ba755f9c2a90",
+    "url": "/react-news-stand/index.html"
+  },
+  {
+    "revision": "19acf1785dcebf74e1b0",
+    "url": "/react-news-stand/static/css/main.247a58eb.chunk.css"
+  },
+  {
+    "revision": "c39d99f022e4d6faf084",
+    "url": "/react-news-stand/static/js/2.a288334a.chunk.js"
+  },
+  {
+    "revision": "0d1a1ea291563329bcf84afb677757e8",
+    "url": "/react-news-stand/static/js/2.a288334a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19acf1785dcebf74e1b0",
+    "url": "/react-news-stand/static/js/main.c79dd7f7.chunk.js"
+  },
+  {
+    "revision": "9216075194968a7b5a14",
+    "url": "/react-news-stand/static/js/runtime-main.ccaac166.js"
+  }
+]);
